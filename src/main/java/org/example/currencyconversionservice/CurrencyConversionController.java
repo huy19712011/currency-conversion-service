5b0a9a65773461d8b1bf4a66de8d3d314590e57a
huy19712011 <35172019+huy19712011@users.noreply.github.com>
2026-09-23 07:06:45 +0700

    v115: Invoking Currency Exchange from Currency Conversion Microservice, using RestClient

--- src/main/java/org/example/currencyconversionservice/CurrencyConversionController.java
+++ src/main/java/org/example/currencyconversionservice/CurrencyConversionController.java
@@ -1,14 +1,34 @@
 package org.example.currencyconversionservice;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestClient;
 
 import java.math.BigDecimal;
 import java.util.HashMap;
 
+@Configuration(proxyBeanMethods = false)
+class RestClientConfiguration {
+
+    @Bean
+    RestClient restClient() {
+        return RestClient.builder().build();
+    }
+}
+
+
 @RestController
 public class CurrencyConversionController {
+
+    private final RestClient restClient;
+
+    public CurrencyConversionController(RestClient restClient) {
+        this.restClient = restClient;
+    }
 
     @GetMapping("/currency-conversion/from/{from}/to/{to}/quantity/{quantity}")
     public CurrencyConversion calculateCurrencyConversion(
@@ -17,7 +37,22 @@ public class CurrencyConversionController {
             @PathVariable BigDecimal quantity
     ) {
 
-        return new CurrencyConversion(10001L, from, to, quantity, BigDecimal.ONE, BigDecimal.ONE, "");
+        //return new CurrencyConversion(10001L, from, to, quantity, BigDecimal.ONE, BigDecimal.ONE, "");
+
+        HashMap<String, String> uriVariables = new HashMap<>();
+        uriVariables.put("from", from);
+        uriVariables.put("to", to);
+        CurrencyConversion currencyConversion = restClient.get()
+                .uri("http://localhost:8000/currency-exchange/from/{from}/to/{to}", uriVariables)
+                .retrieve()
+                .body(CurrencyConversion.class);
+
+        return new CurrencyConversion(currencyConversion.getId(),
+                from, to, quantity,
+                currencyConversion.getConversionMultiple(),
+                quantity.multiply(currencyConversion.getConversionMultiple()),
+                currencyConversion.getEnvironment() + " " + "rest client");
 
     }
 }
+
